29fb1aeaf54bd311a7bfee98b0c32c1e427d5316
Sean <writeseanhong@gmail.com>
2022-03-14 03:47:43 -0500

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, TextField, Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      sx={{
+        margin: "3em auto",
+        textAlign: "center",
+      }}
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <br />
+      <Button>Click me</Button>
+    </Box>
   );
 }
 
