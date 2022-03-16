08a7a640d1cd84aa43a1d3147cbb4c88f1101f69
Sean <writeseanhong@gmail.com>
2022-03-16 02:28:53 -0500

    add test for bad url

--- api/test_api_imagesaver.py
+++ api/test_api_imagesaver.py
@@ -6,7 +6,8 @@ import json;
 def test_post_route_success():
   app = Flask(__name__);
   app.register_blueprint(imagesaver_post, url_prefix="")
-
+  app.config['DEBUG'] = True
+  app.config['TESTING'] = True
   client =  app.test_client();
   url = "/imagesaver"; 
 
@@ -19,3 +20,17 @@ def test_post_route_success():
   }
   response = client.post(url, json=mock_request_data, headers=mock_headers);
   assert response.status_code == 200;
+
+def test_post_route_failure():
+  app = Flask(__name__);
+  app.register_blueprint(imagesaver_post, url_prefix="")
+  app.config['DEBUG'] = True
+  app.config['TESTING'] = True
+  client =  app.test_client();
+  url = "/imagesaver"; 
+
+  mock_request_data = {
+    "url": "http://asdfasdf"
+  }
+  response = client.post(url, json=mock_request_data);
+  assert response.status_code == 500;
