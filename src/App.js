2283856b636cfdce1872d3620e4b13cb87cfff1e
Sean <writeseanhong@gmail.com>
2022-04-17 22:12:47 -0500

    add app bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Form from "./components/Form";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <Form />
     </div>
   );
